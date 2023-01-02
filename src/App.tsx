d0c2d29597fbe415beafcb1d3e0a15be84e391ea
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-01-02 18:58:00 +0300

    created checkWinner func

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,17 @@ function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [xPlaying, setXPlaying] = useState(true)
 
+  const WIN_CONDITIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+
   function clickHandler(boxId: number) {
     const updatedBoard = board.map((el, idx) => {
       if (idx === boxId) {
@@ -16,8 +27,20 @@ function App() {
       }
     })
 
+    checkWinner(updatedBoard)
     setBoard(updatedBoard)
     setXPlaying((prev) => !prev)
+  }
+
+  const checkWinner = (board: string[]) => {
+    for (let i = 0; i < WIN_CONDITIONS.length; i++) {
+      const [x, y, z] = WIN_CONDITIONS[i]
+
+      if (board[x] && board[x] === board[y] && board[y] === board[z]) {
+        console.log(board[x])
+        return board[x]
+      }
+    }
   }
 
   return (
